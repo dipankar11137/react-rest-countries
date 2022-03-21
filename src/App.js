87ea02137c8ec1227dd0b64f0b6cd858c54bc206
Dipankar <dipankar15-11137@diu.edu.bd>
2022-03-21 20:34:11 +0600

    new file add

--- src/App.js
+++ src/App.js
@@ -16,12 +16,25 @@ function LoadCountries() {
   useEffect(() => {
     fetch('https://restcountries.com/v2/all')
       .then(res => res.json())
-      .then(data => console.log(data))
+      .then(data => setCountries(data))
   }, [])
 
   return (
     <div>
       <h1>Visiting Every counter of the world</h1>
+      <h2>Country length : {countries.length}</h2>
+      {
+        countries.map(country => <Country name={country.name} population={country.population}></Country>)
+      }
+    </div>
+  )
+}
+
+function Country(props) {
+  return (
+    <div>
+      <h2>Name : {props.name}</h2>
+      <h4>Population : {props.population}</h4>
     </div>
   )
 }
