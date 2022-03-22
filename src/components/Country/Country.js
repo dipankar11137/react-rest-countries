9f96be07cd6b35404d967cc72ff72df31283c942
Dipankar <dipankar15-11137@diu.edu.bd>
2022-03-22 09:30:36 +0600

    load all data

--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import './Country.css'
 
-const Country = () => {
+const Country = (props) => {
+    console.log(props.country)
     return (
-        <div>
-            <h2>country is other file</h2>
+        <div className='country'>
+            <h2>Country name : {props.country.name.common}</h2>
+            <p>Population : {props.country.population}</p>
+            <p>Region : {props.country.region}</p>
+            <p>Area : {props.country.area}</p>
         </div>
     );
 };
